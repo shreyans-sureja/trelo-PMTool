7b3acd8b8151b3d387443686ec28c1fe02826bb1
shreyans-sureja <shreyanssureja@gmail.com>
2021-01-12 21:44:56 +0530

    Project api created

---
+++ PPMToolFullStack/src/main/java/io/trelo/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.trelo.ppmtool.repositories;
+
+import io.trelo.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
