1071dd19502af28bf133ad6eff0d2cbb5bd70791
shreyans-sureja <shreyanssureja@gmail.com>
2021-01-12 22:44:06 +0530

    custom error message controller added

--- PPMToolFullStack/src/main/java/io/trelo/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/trelo/ppmtool/web/ProjectController.java
@@ -2,13 +2,22 @@ package io.trelo.ppmtool.web;
 
 import io.trelo.ppmtool.domain.Project;
 import io.trelo.ppmtool.services.ProjectService;
+import io.trelo.ppmtool.services.ValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,8 +26,17 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private ValidationErrorService validationErrorService;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap = validationErrorService.ValidationService(result);
+
+        if(errorMap!=null)
+            return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
